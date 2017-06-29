f9ffe431c50b5dfc04ccbed95ed85aaaf8c11ca8
mahilumphil <phil.yupte@gmail.com>
2017-06-29 09:30:52 +0800

    Modified changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+
 // import 'materialize-css';
 // import { MaterializeModule } from 'angular2-materialize';
 
